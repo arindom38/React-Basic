7861dae8ab948e445d50a4cf5dcaa2e2b8bf6188
Arindom Kundu <arindomkundu18@gmail.com>
2021-06-28 19:38:15 +0600

    working with component and templates

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+//A component which will be render inside index.js
+function App() { //anything inside this function before return is js 
+  const title = "Welcome to react world"
+  const likes = 50 //while rending all types of varaibel type is converted to string
+  const car = { name:'Lambogini',body:'suv',MPL:'5'} //if only object is printed it will cause error
+  const arr = [1,2,4.5]
+  const link = "https//www.google.com"
+  return ( //return must return only single element not multiple
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <h1>{title}</h1>
+        <p>{likes}</p>
+        <p>{car.name}</p>
+        {/* <p>{car}</p> this will calsue error*/}
+        <p>{arr}</p>
+        <a href={link}>Google Site</a>
+        <p>{Math.random()*10}</p>
+      </div>
     </div>
+    // {/* <p></p> */} this will create error as only one element can be returned
   );
 }
 
--- my-blog/src/index.js
+++ my-blog/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+//This function render all the components and add them inside the root
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+  {/* All the components want to render */}
+    <App /> 
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root') // index.html the has div of root id
 );
 
 // If you want to start measuring performance in your app, pass a function
