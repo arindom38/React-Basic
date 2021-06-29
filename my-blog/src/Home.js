ad20bf494165f7bed9b872f88257e60fcde94c71
Arindom Kundu <arindomkundu18@gmail.com>
2021-06-29 17:57:55 +0600

    conditional error handling

--- my-blog/src/Home.js
+++ my-blog/src/Home.js
@@ -5,17 +5,25 @@ const Home = () => {
     //output lists
     const [blogs, setBlog] = useState(null)
     const [isLoading, setIsLoading] = useState(true)
-
+    const [errorMssg, setErrorMssg] = useState(null)
 
     useEffect(() => {
         setTimeout(() => { //timeout is used for just simulating real fetching time
             fetch(' http://localhost:8000/blogs')
-                .then(result => {
-                    return result.json()
+                .then(res => {
+                    if (!res.ok) {
+                        throw Error('could not find the resources') //so it goes to catch scope
+                    }
+                    return res.json()
                 })
                 .then(data => {
                     setBlog(data)
                     setIsLoading(false)
+                    setErrorMssg(null)
+                })
+                .catch(err => {
+                    setIsLoading(false)
+                    setErrorMssg(err.message)
                 })
         }, 1000)
     }, [])
@@ -23,6 +31,7 @@ const Home = () => {
 
     return (
         <div className="home">
+            {errorMssg && <div className="error-mssg">{errorMssg}</div>}
             {isLoading && <div className="loading">Loading........</div>}
             {blogs && <BlogList blogs={blogs} title="All blogs" />}
         </div>
