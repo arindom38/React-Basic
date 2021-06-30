30aeb8d15e934bbf07b91d0831d48bf971a9d09c
Arindom Kundu <arindomkundu18@gmail.com>
2021-06-30 08:40:13 +0600

    routes for different page

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -1,13 +1,17 @@
 import Navbar from './navbar';
 import Home from './Home';
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 //root component which will be render inside index.js
 function App() {
   return (
     <div className="App">
       <Navbar />
       <div className="content">
-        <Home />
+        <Router>
+          <Switch path="/"> {/* Route for only home page*/}
+            <Home />
+          </Switch>
+        </Router>
       </div>
     </div>
   );
