ceee8c499e9d04f9d3c95bc8b4859cb71f318814
Arindom Kundu <arindomkundu18@gmail.com>
2021-06-30 09:33:16 +0600

    Added Router Link

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -1,4 +1,4 @@
-import Navbar from './navbar';
+import Navbar from './Navbar';
 import Home from './Home';
 import Create from './Create';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
--- my-blog/src/navbar.js
+++ my-blog/src/navbar.js
@@ -1,3 +1,4 @@
+import {Link} from "react-router-dom"
 // type 'sfc' then enter this statless function will be created
 //this is a arrow function for a component but normal function can also be created like in app.js
 const Navbar = () => {
@@ -5,10 +6,10 @@ const Navbar = () => {
         <nav className="navbar">
             <h1>The React Blog</h1>
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/Create">New Blog</a>
-                <a href="/Myblog">My Blog</a>
-                <a href="/Contact">Contact</a>
+                <Link to="/">Home</Link> {/* Link will not send req to server, rather link to the route in app.js*/}
+                <Link to="/Create">New Blog</Link>
+                <Link to="/Myblog">My Blog</Link>
+                <Link to="/Contact">Contact</Link>
             </div>
         </nav>
     );
