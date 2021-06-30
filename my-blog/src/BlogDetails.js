bfb086c541badaca4553917fa79c357d180e0d37
Arindom Kundu <arindomkundu18@gmail.com>
2021-06-30 23:45:21 +0600

    Added delete function

--- my-blog/src/BlogDetails.js
+++ my-blog/src/BlogDetails.js
@@ -1,9 +1,20 @@
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
     const { id } = useParams()
     const { data: blog, isLoading, errorMssg } = useFetch(process.env.REACT_APP_DB_URL+'/'+ id)
+    const history = useHistory()
+
+    const handleClick = () => {
+        fetch(process.env.REACT_APP_DB_URL+'/'+ blog.id,{
+            method: "DELETE"
+        })
+        .then(()=>{
+            history.push("/")
+        })
+    }
+    
     return (
         <div className="blog-details">
             {errorMssg && <div className="error-mssg">{errorMssg}</div>}
@@ -13,6 +24,7 @@ const BlogDetails = () => {
                     <h2>{blog.title}</h2>
                     <h4>Written By {blog.author}</h4>
                     <p>{blog.body}</p>
+                    <button onClick={handleClick}>Delete</button>
                 </article>
             )}
         </div>
