31c8b07ff8623a0058e9efd1844421dfacd53385
Arindom Kundu <arindomkundu18@gmail.com>
2021-06-30 08:55:41 +0600

    Multuple routes handle

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -1,5 +1,6 @@
 import Navbar from './navbar';
 import Home from './Home';
+import Create from './Create';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 //root component which will be render inside index.js
 function App() {
@@ -8,9 +9,12 @@ function App() {
       <div className="App">
         <Navbar /> {/*no routes added so this will be avialable in every page*/}
         <div className="content">
-          <Switch >
-            <Route path="/"> {/* Route for only home page*/}
+          <Switch > {/*it routes to one page at time, without it all routes will be accessed*/}
+            <Route exact path="/"> {/* Route for only home page, exact property match the path exactly or it will math the first character*/}
               <Home />
+            </Route>
+            <Route exact path="/create"> {/* Route for only home page*/}
+              <Create/>
             </Route>
           </Switch>
         </div>
